18b19045152be14eb1845b3224b7ead1dee5b5ca
Konstantin-Ustinov <ustinov.kn@mail.ru>
2022-08-01 14:30:00 +0300

    first homework is done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import  java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
+        task2();
+        task3();
+        task4();
+        task5();
+    }
 
+    public static void task1() {
         // Task 1
         boolean isEmpty = false;
         byte thisByte = 3;
@@ -12,16 +18,18 @@ public class Main {
         float thisFloat = 4.35f;
         double thisDouble = 5.543211;
         char thisChar = 'c';
+    }
 
-        // Task 2
+    public static void task2() {
         System.out.println("Задание 2");
         double boxer1 = 78.2;
         double boxer2 = 72.7;
 
         System.out.println("Общий вес двух бойцов: " + (boxer1 + boxer2));
         System.out.println("Разница в весе двух бойцов: " + (boxer1 - boxer2));
+    }
 
-        // Task 3
+    public static void task3() {
         System.out.println("Задание 3");
         int oneBananaGram = 80;
         int ml100MilkGram = 105;
@@ -32,11 +40,12 @@ public class Main {
         int icecreamCount = 2;
         int eggCount = 1;
         double sumAllKg = (oneBananaGram * bananaCount + ml100MilkGram * milkCount + oneBriquetteIcecreamGram *
-                            icecreamCount + oneEggGram * eggCount) / 1000.0;
+                icecreamCount + oneEggGram * eggCount) / 1000.0;
 
         System.out.println("Общий вес еды: " + sumAllKg + "кг");
+    }
 
-        // Task 4
+    public static void task4() {
         System.out.println("Задание 4");
 
         int target = 7;
@@ -46,12 +55,13 @@ public class Main {
         double dayCount2 = target / weightPerDay2;
 
         System.out.println("Количество дней для похудения на " + target + "кг, если худеть по " +
-                                (weightPerDay1 * 1000) + "гр в день: " + dayCount1);
+                (weightPerDay1 * 1000) + "гр в день: " + dayCount1);
         System.out.println("Количество дней для похудения на " + target + "кг, если худеть по " +
-                                (weightPerDay2 * 1000) + "гр в день: " + dayCount2);
+                (weightPerDay2 * 1000) + "гр в день: " + dayCount2);
         System.out.println("Среднее количество дней для похудения на " + target + "кг: " + ((dayCount1 + dayCount2) / 2));
+    }
 
-        // Task 5
+    public static void task5() {
         System.out.println("Задание 5");
 
         int salaryMasha = 67760;
@@ -63,10 +73,10 @@ public class Main {
         double newSalaryKristina = Math.round(salaryKristina * percentUpSalary);
 
         System.out.println("Зарплата Маши стала: " + newSalaryMasha +
-                                ". Годовой доход вырос на: " + (newSalaryMasha - salaryMasha));
+                ". Годовой доход вырос на: " + (newSalaryMasha - salaryMasha));
         System.out.println("Зарплата Дениса стала: " + newSalaryDenis +
-                                ". Годовой доход вырос на: " + (newSalaryDenis - salaryDenis));
+                ". Годовой доход вырос на: " + (newSalaryDenis - salaryDenis));
         System.out.println("Зарплата Кристины стала: " + newSalaryKristina +
-                                ". Годовой доход вырос на: " + (newSalaryKristina - salaryKristina));
+                ". Годовой доход вырос на: " + (newSalaryKristina - salaryKristina));
     }
 }
